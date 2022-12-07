4a5a5012ff5b6f0316a60155153d07d9e46b3e80
JP Sison <johnpatrick.sison.iics@ust.edu.ph>
2022-12-07 15:44:00 +0800

    dfas

--- index.php
+++ index.php
@@ -1,36 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'includes/connect.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GitFit | Login</title>
+	if($_SESSION['customerSID']==session_id())
+	{
+		?>
 
-</head>
 
-<body>
+<?php
+	}
+	else
+	{
+		if($_SESSION['adminSID']==session_id())
+		{
+			header("location:admin-page.php");		
+		}
+		else{
+			header("location:login.php");
+		}
+	}
+?>
+    
 
-    <div class="su-content">
-        <div class="signup">
-            <h1 class="su-title">Log in</h1>
-        </div><br class="hiddenbr">
-        <div class="su-details">
-            <br><br>
-            <div class="su-email">
-                <p class="su-text">Email:</p>
-                <input id="email" type="text" class="fields" placeholder="email@gmail.com" tabindex="1">
-            </div><br>
-            <div class="su-pass">
-                <p class="su-text">Password:</p>
-                <input id="password" type="password" class="fields" placeholder="password" tabindex="2">
-            </div><br>
-            <div class="su-button">
-                <button id="loginbtn" type="button" tabindex="3">Login</button>
-            </div>
-        </div>
-    </div>
-
-</body>
-
-</html>
---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GitFit | Login</title>
+
+</head>
+
+<body>
+    <?php 
+    include 'db/connection.php';
+    ?>
+    <div class="su-content">
+        <div class="signup">
+            <h1 class="su-title">Log in</h1>
+        </div><br class="hiddenbr">
+        <div class="su-details">
+            <br><br>
+            <div class="su-email">
+                <p class="su-text">Email:</p>
+                <input id="email" type="text" class="fields" placeholder="email@gmail.com" tabindex="1">
+            </div><br>
+            <div class="su-pass">
+                <p class="su-text">Password:</p>
+                <input id="password" type="password" class="fields" placeholder="password" tabindex="2">
+            </div><br>
+            <div class="su-button">
+                <button id="loginbtn" type="button" tabindex="3">Login</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
